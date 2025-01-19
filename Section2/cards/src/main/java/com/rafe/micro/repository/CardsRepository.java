50e739d4997dd505a4c61abd2388e630b9ec3124
nakhuda1 <rafe.nakhuda@here.com>
2025-01-19 12:49:17 +0530

    Cards Microservice completed

---
+++ Section2/cards/src/main/java/com/rafe/micro/repository/CardsRepository.java
@@ -0,0 +1,16 @@
+package com.rafe.micro.repository;
+
+import com.rafe.micro.entity.Cards;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface CardsRepository extends JpaRepository<Cards, Long> {
+
+    Optional<Cards> findByMobileNumber(String mobileNumber);
+
+    Optional<Cards> findByCardNumber(String cardNumber);
+
+}
---
+++ Section2/cards/src/test/java/com/rafe/micro/CardsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rafe.micro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CardsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
